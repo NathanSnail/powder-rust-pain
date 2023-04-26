0acc807671b30c233dd6fbb780c663ec5f776bc5
Nahtan <nathan.titirangi@gmail.com>
2023-04-27 08:50:18 +1200

    new stuff

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 fn main() {
     println!("Hello, wordle!");
 	println!("wow power");
+	let mut loop_val: isize = 1;
+	while (loop_val <= 100) {
+		print!("{loop_val}");
+		loop_val += 1;
+	}
+	println!("build me")
 }
