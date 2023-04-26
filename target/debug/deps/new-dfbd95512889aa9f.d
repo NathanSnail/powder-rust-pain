e6f1e5b74cece92ff52488a92e03693c020f5fd4
vboxuser <vboxuser@Debian.myguest.virtualbox.org>
2023-04-27 08:54:10 +1200

    gitig no more

--- target/debug/deps/new-dfbd95512889aa9f.d
+++
@@ -1,5 +0,0 @@
-c:\Users\natha\Documents\code\pain\powder-rust-pain\target\debug\deps\new-dfbd95512889aa9f.rmeta: src\main.rs
-
-c:\Users\natha\Documents\code\pain\powder-rust-pain\target\debug\deps\new-dfbd95512889aa9f.d: src\main.rs
-
-src\main.rs:
