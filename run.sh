f123ac683bcdc7e99ed51572de4fe48c9ed05f6e
vboxuser <vboxuser@Debian.myguest.virtualbox.org>
2023-04-27 08:53:34 +1200

    gitig no more

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 git fetch
+ping -c 4 127.0.0.1 > a.txt
+git reset --hard origin/main
+ping -c 4 127.0.0.1 > a.txt
+git fetch
+ping -c 4 127.0.0.1 > a.txt
 cargo build --target x86_64-pc-windows-gnu
+ping -c 4 127.0.0.1 > a.txt
 #git add *
 #git commit -m "yes"
+ping -c 4 127.0.0.1 > a.txt
 gh release upload --clobber test target/x86_64-pc-windows-gnu/debug/new.exe
