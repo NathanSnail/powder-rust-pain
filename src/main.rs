f014a7589b95f2b0d8dee7caf5b093c29576b7bc
Nahtan <nathan.titirangi@gmail.com>
2023-04-27 12:31:40 +1200

    hammy

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,18 @@ use vulkano::instance::{Instance, InstanceCreateInfo};
 
 fn main() {
 	
-let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
-let instance = Instance::new(library, InstanceCreateInfo::default())
-    .expect("failed to create instance");
+	let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
+	let instance = Instance::new(library, InstanceCreateInfo::default())
+		.expect("failed to create instance");
 	let physical_device = instance
-    .enumerate_physical_devices()
-    .expect("could not enumerate devices")
-    .next()
-    .expect("no devices available");
+		.enumerate_physical_devices()
+		.expect("could not enumerate devices")
+		.next()
+		.expect("no devices available");
 	println!("ham")
 	for family in physical_device.queue_family_properties() {
-    println!("Found a queue family with {:?} queue(s)", family.queue_count);
-}
+		println!("Found a queue family with {:?} queue(s)", family.queue_count);
+	}
 
 
     /*println!("Hello, wordle!");
