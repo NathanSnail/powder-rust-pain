deb037f95eb86ba15cd0c17f6916281bdbb09a59
Nahtan <nathan.titirangi@gmail.com>
2023-04-27 12:25:13 +1200

    hi

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,15 @@ fn main() {
 let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
 let instance = Instance::new(library, InstanceCreateInfo::default())
     .expect("failed to create instance");
+	let physical_device = instance
+    .enumerate_physical_devices()
+    .expect("could not enumerate devices")
+    .next()
+    .expect("no devices available");
+	for family in physical_device.queue_family_properties() {
+    println!("Found a queue family with {:?} queue(s)", family.queue_count);
+}
+
 
     /*println!("Hello, wordle!");
 	println!("wow power");
