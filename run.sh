a2b9d7214a23318c42c7c56b7a6943b38faf55d7
Nahtan <nathan.titirangi@gmail.com>
2023-04-28 13:28:32 +1200

    hax

--- run.sh
+++ run.sh
@@ -2,3 +2,6 @@ git fetch
 ping -c 2 127.0.0.1 > a.txt
 git pull
 ping -c 2 127.0.0.1 > a.txt
+cargo build --target x86_64-pc-windows-gnu > log.txt
+gh release upload --clobber test target/x86_64-pc-windows-gnu/debug/new.exe
+pinc -c 10 127.0.0.1 > a.txt
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,85 @@
-use vulkano::VulkanLibrary;
+use vulkano::buffer::{Buffer, BufferCreateInfo, BufferUsage};
+use vulkano::command_buffer::allocator::{
+    StandardCommandBufferAllocator, StandardCommandBufferAllocatorCreateInfo,
+};
+use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBufferUsage, CopyBufferInfo};
+use vulkano::device::{Device, DeviceCreateInfo, QueueCreateInfo, QueueFlags};
 use vulkano::instance::{Instance, InstanceCreateInfo};
-use vulkano::device::{QueueFlags, Device, DeviceCreateInfo, QueueCreateInfo};
+use vulkano::memory::allocator::{AllocationCreateInfo, MemoryUsage, StandardMemoryAllocator};
+use vulkano::VulkanLibrary;
+
+struct test {
+    first: i32,
+    second: i32,
+    res: i32,
+}
 
 fn main() {
-	
-	let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
-	let instance = Instance::new(library, InstanceCreateInfo::default())
-		.expect("failed to create instance");
-	let physical_device = instance
-		.enumerate_physical_devices()
-		.expect("could not enumerate devices")
-		.next()
-		.expect("no devices available");
-	println!("ham");
-	for family in physical_device.queue_family_properties() {
-		println!("Found a queue family with {:?} queue(s)", family.queue_count);
-	}
-	let queue_family_index = physical_device
-    .queue_family_properties()
-    .iter()
-    .enumerate()
-    .position(|(_queue_family_index, queue_family_properties)| {
-        queue_family_properties.queue_flags.contains(QueueFlags::GRAPHICS)
-    })
-    .expect("couldn't find a graphical queue family") as u32;
-	let (device, mut queues) = Device::new(
-		physical_device,
-		DeviceCreateInfo {
-			// here we pass the desired queue family to use by index
-			queue_create_infos: vec![QueueCreateInfo {
-			queue_family_index,
+    let mut rng = rand::thread_rng();
+    let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
+    let instance =
+        Instance::new(library, InstanceCreateInfo::default()).expect("failed to create instance");
+    let physical_device = instance
+        .enumerate_physical_devices()
+        .expect("could not enumerate devices")
+        .next()
+        .expect("no devices available");
+    println!("ham");
+    for family in physical_device.queue_family_properties() {
+        println!(
+            "Found a queue family with {:?} queue(s)",
+            family.queue_count
+        );
+    }
+    let queue_family_index = physical_device
+        .queue_family_properties()
+        .iter()
+        .enumerate()
+        .position(|(_queue_family_index, queue_family_properties)| {
+            queue_family_properties
+                .queue_flags
+                .contains(QueueFlags::GRAPHICS)
+        })
+        .expect("couldn't find a graphical queue family") as u32;
+    let (device, mut queues) = Device::new(
+        physical_device,
+        DeviceCreateInfo {
+            // here we pass the desired queue family to use by index
+            queue_create_infos: vec![QueueCreateInfo {
+                queue_family_index,
+                ..Default::default()
+            }],
             ..Default::default()
-        }],
-		..Default::default()
-		},
-	)
-	.expect("failed to create device");
-	println!("Device acquired");
-	
-	// for q in queues {
-	// 	println!("{q:?}");
-	// }
+        },
+    )
+    .expect("failed to create device");
+    println!("Device acquired");
+    let command_buffer_allocator = StandardCommandBufferAllocator::new(
+        device.clone(),
+        StandardCommandBufferAllocatorCreateInfo::default(),
+    );
+    let memory_allocator = StandardMemoryAllocator::new_default(device.clone());
+    let data: test = test {
+        first: 5,
+        second: 7,
+        res: 10,
+    };
+    let buffer = Buffer::from_data(
+        &memory_allocator,
+        BufferCreateInfo {
+            usage: BufferUsage::UNIFORM_BUFFER,
+            ..Default::default()
+        },
+        AllocationCreateInfo {
+            usage: MemoryUsage::Upload,
+            ..Default::default()
+        },
+        data,
+    )
+    .expect("failed to create buffer");
+    println!("buffer (pogger)");
+
+    // for q in queues {
+    // 	println!("{q:?}");
+    // }
 }
