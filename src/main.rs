22d27618f72877e84902927b67f0c4b680e87971
Nahtan <nathan.titirangi@gmail.com>
2023-04-28 17:12:53 +1200

    it may work?

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@ use vulkano::command_buffer::allocator::{
     StandardCommandBufferAllocator, StandardCommandBufferAllocatorCreateInfo,
 };
 use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBufferUsage, CopyBufferInfo};
+use vulkano::descriptor_set::allocator::StandardDescriptorSetAllocator;
+use vulkano::descriptor_set::{PersistentDescriptorSet, WriteDescriptorSet};
 use vulkano::device::{Device, DeviceCreateInfo, QueueCreateInfo, QueueFlags};
 use vulkano::instance::{Instance, InstanceCreateInfo};
 use vulkano::memory::allocator::{AllocationCreateInfo, MemoryUsage, StandardMemoryAllocator};
+use vulkano::pipeline::{ComputePipeline, PipelineBindPoint};
 use vulkano::VulkanLibrary;
 use vulkano_shaders::*;
 
@@ -18,15 +21,17 @@ struct TestStruct {
 }
 
 mod cs {
-    vulkano_shaders::shader!{
+    vulkano_shaders::shader! {
         ty: "compute",
         src: r"
             #version 460
 
             layout(local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
+			
+
 
             layout(set = 0, binding = 0) buffer Data {
-                uint data[];
+                uint[] data;
             } buf;
 
             void main() {
@@ -47,13 +52,14 @@ fn main() {
         .next()
         .expect("no devices available");
     let name = &physical_device.properties().device_name;
-	println!("{name:?}");
+    println!("{name:?}");
     for family in physical_device.queue_family_properties() {
         println!(
             "Found a queue family with {:?} queue(s)",
             family.queue_count
         );
     }
+
     let queue_family_index = physical_device
         .queue_family_properties()
         .iter()
@@ -64,6 +70,7 @@ fn main() {
                 .contains(QueueFlags::GRAPHICS)
         })
         .expect("couldn't find a graphical queue family") as u32;
+
     let (device, mut queues) = Device::new(
         physical_device,
         DeviceCreateInfo {
@@ -77,17 +84,21 @@ fn main() {
     )
     .expect("failed to create device");
     println!("Device acquired");
+
     let command_buffer_allocator = StandardCommandBufferAllocator::new(
         device.clone(),
         StandardCommandBufferAllocatorCreateInfo::default(),
     );
     let memory_allocator = StandardMemoryAllocator::new_default(device.clone());
+
     let data: TestStruct = TestStruct {
         first: 5,
         second: 7,
         res: 10,
     };
-    let buffer = Buffer::from_data(
+
+    let data2 = 0..64;
+    let buffer = Buffer::from_iter(
         &memory_allocator,
         BufferCreateInfo {
             usage: BufferUsage::UNIFORM_BUFFER,
@@ -97,10 +108,71 @@ fn main() {
             usage: MemoryUsage::Upload,
             ..Default::default()
         },
-        data,
+        data2,
     )
     .expect("failed to create buffer");
     println!("buffer (pogger)");
+
+    let shader = cs::load(device.clone()).expect("failed to create shader module");
+
+    let compute_pipeline = ComputePipeline::new(
+        device.clone(),
+        shader.entry_point("main").unwrap(),
+        &(),
+        None,
+        |_| {},
+    )
+    .expect("failed to create compute pipeline");
+
+    let descriptor_set_allocator = StandardDescriptorSetAllocator::new(device.clone());
+    let pipeline_layout = compute_pipeline.layout();
+    let descriptor_set_layouts = pipeline_layout.set_layouts();
+    let descriptor_set_layout_index = 0;
+    let descriptor_set_layout = descriptor_set_layouts
+        .get(descriptor_set_layout_index)
+        .unwrap();
+    let descriptor_set = PersistentDescriptorSet::new(
+        &descriptor_set_allocator,
+        descriptor_set_layout.clone(),
+        [WriteDescriptorSet::buffer(0, buffer.clone())], // 0 is the binding
+    )
+    .unwrap();
+
+    let command_buffer_allocator = StandardCommandBufferAllocator::new(
+        device.clone(),
+        StandardCommandBufferAllocatorCreateInfo::default(),
+    );
+    let mut command_buffer_builder = AutoCommandBufferBuilder::primary(
+        &command_buffer_allocator,
+        queue.queue_family_index(),
+        CommandBufferUsage::OneTimeSubmit,
+    )
+    .unwrap();
+
+    let work_group_counts = [1, 1, 1];
+
+    command_buffer_builder
+        .bind_pipeline_compute(compute_pipeline.clone())
+        .bind_descriptor_sets(
+            PipelineBindPoint::Compute,
+            compute_pipeline.layout().clone(),
+            descriptor_set_layout_index as u32,
+            descriptor_set,
+        )
+        .dispatch(work_group_counts)
+        .unwrap();
+
+    let command_buffer = command_buffer_builder.build().unwrap();
+
+    let future = sync::now(device.clone())
+        .then_execute(queue.clone(), command_buffer)
+        .unwrap()
+        .then_signal_fence_and_flush()
+        .unwrap();
+
+    future.wait(None).unwrap();
+    let content = data_buffer.read().unwrap();
+	println!("{content:?}");
 
     // for q in queues {
     // 	println!("{q:?}");
