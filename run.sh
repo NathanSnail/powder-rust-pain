c9e756e1ce705079d740a6eef0554f4b0cb21604
vboxuser <vboxuser@Debian.myguest.virtualbox.org>
2023-04-28 16:18:05 +1200

    vbox

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@ ping -c 2 127.0.0.1 > a.txt
 git pull
 ping -c 2 127.0.0.1 > a.txt
 cargo build --target x86_64-pc-windows-gnu > log.txt
+ping -c 2 127.0.0.1 > a.txt
 gh release upload --clobber test target/x86_64-pc-windows-gnu/debug/new.exe
-pinc -c 10 127.0.0.1 > a.txt
+ping -c 2 127.0.0.1 > a.txt
