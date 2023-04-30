b3493cdbb403098ad982e95fee9a928b2e44a22d
vboxuser <vboxuser@Debian.myguest.virtualbox.org>
2023-05-01 08:29:40 +1200

    vbox

--- loop.py
+++ loop.py
@@ -1,11 +1,23 @@
 import os
 import time
+import subprocess
+import tempfile
 t = time.time()
 while True:
-	try:
+#	try:
 		print(time.time()-t)
 		t = time.time()
-		os.system("./run.sh")
+		with tempfile.TemporaryFile() as tempf:
+
+			proc = subprocess.Popen(["sh","./run.sh"], stdout=tempf)
+			proc.wait()
+
+			proc.seek(0)
+			content = tempf.read().replace("\"","\\\"")
+			print(content)
+			os.system(f"gh release edit test -n \"{content}\"")
+		#os.system("./run.sh")
 		time.sleep(1)
-	except:
+#	except:
+		print("error")
 		pass #someone pressed ctrl+c or the internet died, use zz thingy
