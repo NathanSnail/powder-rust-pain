ebb485732642749d26569b23bf4a51730e0f0987
vboxuser <vboxuser@Debian.myguest.virtualbox.org>
2023-05-01 08:44:33 +1200

    aa

--- loop.py
+++ loop.py
@@ -7,16 +7,17 @@ while True:
 #	try:
 		print(time.time()-t)
 		t = time.time()
-		with tempfile.TemporaryFile() as tempf:
+		#with tempfile.TemporaryFile() as tempf:
 
-			proc = subprocess.Popen(["sh","./run.sh"], stdout=tempf)
-			proc.wait()
+			#proc = subprocess.Popen(["sh","./run.sh"], stdout=tempf)
+			
+			#proc.wait()
 
-			proc.seek(0)
-			content = tempf.read().replace("\"","\\\"")
-			print(content)
-			os.system(f"gh release edit test -n \"{content}\"")
-		#os.system("./run.sh")
+			#tempf.seek(0)
+			#content = str(tempf.read()).replace("\"","\\\"")
+			#print("cont: " + content)
+			#os.system(f"gh release edit test -n \"{content}\"")
+		os.system("./run.sh")
 		time.sleep(1)
 #	except:
 		print("error")
