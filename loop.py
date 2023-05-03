a2f261530c1188e2771525a7b881b0d17e17b161
Nahtan <nathan.titirangi@gmail.com>
2023-05-03 19:34:43 +1200

    auto commit

--- loop.py
+++ loop.py
@@ -1,24 +1,9 @@
 import os
 import time
-import subprocess
-import tempfile
 t = time.time()
 while True:
-#	try:
-		print(time.time()-t)
-		t = time.time()
-		#with tempfile.TemporaryFile() as tempf:
-
-			#proc = subprocess.Popen(["sh","./run.sh"], stdout=tempf)
-			
-			#proc.wait()
-
-			#tempf.seek(0)
-			#content = str(tempf.read()).replace("\"","\\\"")
-			#print("cont: " + content)
-			#os.system(f"gh release edit test -n \"{content}\"")
-		os.system("./run.sh")
-		time.sleep(1)
-#	except:
-		print("error")
-		pass #someone pressed ctrl+c or the internet died, use zz thingy
+    print(time.time()-t)
+    t = time.time()
+    os.system("./run.sh")
+    time.sleep(1)
+    print("error")
--- upload.sh
+++
@@ -1,3 +0,0 @@
-cargo build --target x86_64-pc-windows-gnu > log.txt
-gh release edit test -n "$1"
-gh release upload --clobber test target/x86_64-pc-windows-gnu/debug/new.exe
