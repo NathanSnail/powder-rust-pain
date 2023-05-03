b06532e44eb2ef28f7f647ec053f4f1188c8b2aa
Nahtan <nathan.titirangi@gmail.com>
2023-05-03 20:55:24 +1200

    auto commit

--- src/window.rs
+++ src/window.rs
@@ -7,10 +7,10 @@ use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::WindowBuilder;
 
 pub fn window(instance: Arc<Instance>, library: Arc<VulkanLibrary>) {
-    let required_extensions = vulkano_win::required_extensions(&library);
+    let _required_extensions = vulkano_win::required_extensions(&library);
     let event_loop = EventLoop::new(); // ignore this for now
-    let surface = WindowBuilder::new()
-        .build_vk_surface(&event_loop, instance)
+    let _surface = WindowBuilder::new()
+        .build_vk_surface(&event_loop, instance.clone())
         .unwrap();
 
     event_loop.run(|event, _, control_flow| match event {
