83c858eb70234fe381b48aef98735b0ec7a33548
Nahtan <nathan.titirangi@gmail.com>
2023-05-03 21:06:48 +1200

    auto commit

--- src/window.rs
+++ src/window.rs
@@ -2,26 +2,26 @@ use std::sync::Arc;
 use vulkano::instance::Instance;
 use vulkano::{VulkanLibrary};
 use vulkano_win::VkSurfaceBuild;
-use winit::event::{Event, WindowEvent};
-use winit::event_loop::{ControlFlow, EventLoop};
+
+use winit::event_loop::{EventLoop};
 use winit::window::WindowBuilder;
 use vulkano::instance::InstanceCreateInfo;
 
-pub fn window(_instance: Arc<Instance>, _library: Arc<VulkanLibrary>) {
-	let library = vulkano::VulkanLibrary::new().expect("no local Vulkan library/DLL");
+pub fn window(instance: Arc<Instance>, library: Arc<VulkanLibrary>) {
+	// let library = vulkano::VulkanLibrary::new().expect("no local Vulkan library/DLL");
     let required_extensions = vulkano_win::required_extensions(&library);
-    let instance = Instance::new(
-        library,
-        InstanceCreateInfo {
-            enabled_extensions: required_extensions,
-            ..Default::default()
-        },
-    )
-    .expect("failed to create instance");
+    // let instance = Instance::new(
+    //     library,
+    //     InstanceCreateInfo {
+    //         enabled_extensions: required_extensions,
+    //         ..Default::default()
+    //     },
+    // )
+    // .expect("failed to create instance");
 
     let event_loop = EventLoop::new();
-    let surface = WindowBuilder::new()
-        .build_vk_surface(&event_loop, instance.clone())
+    let _surface = WindowBuilder::new()
+        .build_vk_surface(&event_loop, instance)
         .unwrap();
 
 }
