d3d3016e7bb5afd25fdb6483a3da605f5de6a173
Nahtan <nathan.titirangi@gmail.com>
2023-05-05 09:49:39 +1200

    auto commit

--- src/window.rs
+++ src/window.rs
@@ -113,76 +113,76 @@ pub fn window(library: Arc<VulkanLibrary>) {
             .0,
     );
 
-    let (swapchain, images) = {
-        let caps = physical_device
-            .surface_capabilities(&surface, Default::default())
-            .expect("failed to get surface capabilities");
+    // let (swapchain, images) = {
+    //     let caps = physical_device
+    //         .surface_capabilities(&surface, Default::default())
+    //         .expect("failed to get surface capabilities");
 
-        let dimensions = window.inner_size();
-        let composite_alpha = caps.supported_composite_alpha.into_iter().next().unwrap();
-        let image_format = Some(
-            physical_device
-                .surface_formats(&surface, Default::default())
-                .unwrap()[0]
-                .0,
-        );
+    //     let dimensions = window.inner_size();
+    //     let composite_alpha = caps.supported_composite_alpha.into_iter().next().unwrap();
+    //     let image_format = Some(
+    //         physical_device
+    //             .surface_formats(&surface, Default::default())
+    //             .unwrap()[0]
+    //             .0,
+    //     );
 
-        Swapchain::new(
-            device.clone(),
-            surface,
-            SwapchainCreateInfo {
-                min_image_count: caps.min_image_count,
-                image_format,
-                image_extent: dimensions.into(),
-                image_usage: ImageUsage::COLOR_ATTACHMENT,
-                composite_alpha,
-                ..Default::default()
-            },
-        )
-        .unwrap()
-    };
+    //     Swapchain::new(
+    //         device.clone(),
+    //         surface,
+    //         SwapchainCreateInfo {
+    //             min_image_count: caps.min_image_count,
+    //             image_format,
+    //             image_extent: dimensions.into(),
+    //             image_usage: ImageUsage::COLOR_ATTACHMENT,
+    //             composite_alpha,
+    //             ..Default::default()
+    //         },
+    //     )
+    //     .unwrap()
+    // };
 
-    let render_pass = utils::get_render_pass(device.clone(), swapchain);
-    let framebuffers = utils::get_framebuffers(&images, render_pass.clone());
+    // let render_pass = utils::get_render_pass(device.clone(), swapchain);
+    // let framebuffers = utils::get_framebuffers(&images, render_pass.clone());
 
-    let memory_allocator = StandardMemoryAllocator::new_default(device.clone());
+    // let memory_allocator = StandardMemoryAllocator::new_default(device.clone());
 
-    let vertex1 = utils::CPUVertex {
-        position: [-0.5, -0.5],
-    };
-    let vertex2 = utils::CPUVertex {
-        position: [0.0, 0.5],
-    };
-    let vertex3 = utils::CPUVertex {
-        position: [0.5, -0.25],
-    };
-    let vertex_buffer = Buffer::from_iter(
-        &memory_allocator,
-        BufferCreateInfo {
-            usage: BufferUsage::VERTEX_BUFFER,
-            ..Default::default()
-        },
-        AllocationCreateInfo {
-            usage: MemoryUsage::Upload,
-            ..Default::default()
-        },
-        vec![vertex1, vertex2, vertex3],
-    )
-    .unwrap();
+    // let vertex1 = utils::CPUVertex {
+    //     position: [-0.5, -0.5],
+    // };
+    // let vertex2 = utils::CPUVertex {
+    //     position: [0.0, 0.5],
+    // };
+    // let vertex3 = utils::CPUVertex {
+    //     position: [0.5, -0.25],
+    // };
+    // let vertex_buffer = Buffer::from_iter(
+    //     &memory_allocator,
+    //     BufferCreateInfo {
+    //         usage: BufferUsage::VERTEX_BUFFER,
+    //         ..Default::default()
+    //     },
+    //     AllocationCreateInfo {
+    //         usage: MemoryUsage::Upload,
+    //         ..Default::default()
+    //     },
+    //     vec![vertex1, vertex2, vertex3],
+    // )
+    // .unwrap();
 
-    let vs = vs::load(device.clone()).expect("failed to create shader module");
-    let fs = fs::load(device.clone()).expect("failed to create shader module");
+    // let vs = vs::load(device.clone()).expect("failed to create shader module");
+    // let fs = fs::load(device.clone()).expect("failed to create shader module");
 
-    let viewport = Viewport {
-        origin: [0.0, 0.0],
-        dimensions: window_size.into(),
-        depth_range: 0.0..1.0,
-    };
+    // let viewport = Viewport {
+    //     origin: [0.0, 0.0],
+    //     dimensions: window_size.into(),
+    //     depth_range: 0.0..1.0,
+    // };
 
-    let pipeline = utils::get_pipeline(device.clone(), vs, fs, render_pass, viewport);
+    // let pipeline = utils::get_pipeline(device.clone(), vs, fs, render_pass, viewport);
 
-    let _command_buffers =
-        utils::get_command_buffers(&device, &queue, &pipeline, &framebuffers, &vertex_buffer);
+    // let _command_buffers =
+    //     utils::get_command_buffers(&device, &queue, &pipeline, &framebuffers, &vertex_buffer);
 
     event_loop.run(|event, _, control_flow| match event {
         Event::WindowEvent {
